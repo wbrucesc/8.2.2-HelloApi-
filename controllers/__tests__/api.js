2caea6016d90c85881167ba23b7ccecba4dc0d3d
Will Bruce <willtiger13@gmail.com>
2017-09-05 13:30:03 -0400

    test setup

--- controllers/__tests__/api.js
+++ controllers/__tests__/api.js
@@ -7,7 +7,10 @@ describe('GET /api/persons', function(){
     return request(app)
       .get('/api/persons')
       .then(function(response){
+        const people = response.body;
         expect(response.statusCode).toBe(200);
+        expect(people.length).toBe(1);
+        expect(people[0].name).toBe('dan');
       });
 
   });
